15b36c367d118b4ed500eb4681fb137c7e6edb20
akil <ak1lt4hsin@gmail.com>
2024-09-02 01:35:35 +0600

    fixes

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//About Route
 
 Route::get('/about/create', [AboutController::class, 'create']);
 Route::post('/about/store', [AboutController::class, 'store'])->name('about.store');
@@ -14,3 +15,7 @@ Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 Route::get('/about/edit/{id}', [AboutController::class, 'edit'])->name('about.edit');
 Route::put('/about/update/{id}', [AboutController::class, 'update'])->name('about.update');
 Route::delete('/about/delete/{id}', [AboutController::class, 'delete'])->name('about.delete');
+
+//About Route End
+
+//Customer Route
